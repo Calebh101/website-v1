c38f0d90e47ebcf3ebd82a1a18b01cdef834aee7
Caleb <calebharper77@gmail.com>
2025-01-31 20:54:55 -0600

    import

--- host.py
+++ host.py
@@ -1,2 +1,21 @@
-import subprocess
-subprocess.run(["python3", "-m", "http.server", "8020"])
+# what are you doing here
+
+import http.server
+import socketserver
+import os
+
+class handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if not self.path.endswith(".html") and not "." in self.path:
+            possible_html = self.path.lstrip("/") + ".html"
+            if os.path.exists(possible_html):
+                self.path += ".html"
+
+        return super().do_GET()
+
+PORT = 8020
+Handler = handler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"server running on port {PORT}")
+    httpd.serve_forever()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# What is this?
+
+This is my website. Includes host.py, which will host this as a temporary test server.
